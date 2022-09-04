84d799c53932cbee80f66167c79cde87c6ad53e6
parz3val <iamtheparzival@gmail.com>
2022-09-04 19:24:57 +0545

    do : some changes

--- sktcore/src/main.rs
+++ sktcore/src/main.rs
@@ -10,17 +10,18 @@ fn main() {
     println!("Hello, world!");
     let binds  = vec![
         KeyBind {
-            keys: vec![Keyboard::LeftWindows, Keyboard::LeftShift, Keyboard::B],
+            keys: vec![Keyboard::Space, Keyboard::B],
             action: Box::new(|| {
                 start_application("Start Chrome")
             })
         },
         KeyBind {
-            keys: vec![Keyboard::LeftWindows, Keyboard::LeftShift, Keyboard::T],
+            keys: vec![Keyboard::Space, Keyboard::T],
             action : Box::new(|| {
                 start_application("Start WindowsTerminal")
             })
-        }
+        },
+
     ];
     for keybind in binds {
         mki::register_hotkey(&keybind.keys, keybind.action)
@@ -43,3 +44,4 @@ fn start_application(application: &str) {
     Command::new("powershell").arg(application).spawn().expect("Failed to execute command");
 
 }
+
